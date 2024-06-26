d77efba3e350a235826dbefc4193627a3d2f3872
Grzegorz Strączek <grzegorz.straczek@neti-soft.com>
2024-06-26 15:27:38 +0200

    Add links to welcome screen

--- src/components/ui/mainContent.tsx
+++ src/components/ui/mainContent.tsx
@@ -37,17 +37,37 @@ export default function MainContent({
           As part of your DataCap Request Application, you are required to
           complete KYC verification.
         </p>
-        <p>To do so, you'll have to follow these steps:</p>
+        <p>To do so, you&apos;ll have to follow these steps:</p>
         <br />
         <ul className="list-disc list-inside">
           <li className="mb-2">
-            Obtain a <a href="https://passport.gitcoin.co/">Gitcoin Passport</a>
+            Obtain a{" "}
+            <a
+              href="https://passport.gitcoin.co/"
+              className="text-blue-500 underline"
+            >
+              Gitcoin Passport
+            </a>
           </li>
           <li className="mb-2">
-            Ensure your passport score meets the minimum requirement of $
-            {scoreThreshold}
+            Ensure your{" "}
+            <a
+              href="https://support.gitcoin.co/gitcoin-knowledge-base/misc/explorer-passport-guide"
+              className="text-blue-500 underline"
+            >
+              passport score
+            </a>{" "}
+            meets the minimum requirement of ${scoreThreshold}
           </li>
-          <li className="mb-2">Bring the passport to Optimism</li>
+          <li className="mb-2">
+            <a
+              href="https://support.passport.xyz/passport-knowledge-base/using-passport/onchain-passport"
+              className="text-blue-500 underline"
+            >
+              Bring the passport
+            </a>{" "}
+            to Optimism
+          </li>
           <li className="mb-2">
             Confirm your ownership of the passport using your wallet.
           </li>
